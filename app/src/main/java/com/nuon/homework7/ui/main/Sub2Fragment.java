eaee477f024f2587eb62622a196a7ff3e330ee18
Sreynuon <Veansreynuon8@gmail.com>
2024-02-27 12:18:52 +0700

    first commit

---
+++ app/src/main/java/com/nuon/homework7/ui/main/Sub2Fragment.java
@@ -0,0 +1,23 @@
+package com.nuon.homework7.ui.main;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.nuon.homework7.R;
+
+public class Sub2Fragment extends Fragment {
+
+    public Sub2Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
+
---
+++ app/src/main/java/com/nuon/homework7/ui/main/sub2.java
@@ -0,0 +1,23 @@
+package com.nuon.homework7.ui.main;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.nuon.homework7.R;
+
+public class sub2 extends Fragment {
+
+    public sub2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
+
